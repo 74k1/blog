1a4d5d861a19d0feaedcb40172ccf59cc7a88ff9
74k1 <git.t@betsumei.com>
2024-12-10 20:58:49 +0100

    feat(chore): Update HTML templates and add contact page

--- ssg/src/Main.hs
+++ ssg/src/Main.hs
@@ -3,26 +3,22 @@
 import Control.Monad (forM_)
 import Data.List (isPrefixOf, isSuffixOf)
 import Data.Maybe (fromMaybe)
+import Hakyll
 import qualified Data.Text as T
 import qualified Data.Text.Slugger as Slugger
-import Hakyll
 import System.FilePath (takeFileName, (</>))
-import Text.Pandoc (
-  Extension (
-    Ext_fenced_code_attributes,
-    Ext_footnotes,
-    Ext_gfm_auto_identifiers,
-    Ext_implicit_header_references,
-    Ext_smart
-  ),
-  Extensions,
-  ReaderOptions,
-  WriterOptions (writerHighlightStyle),
-  extensionsFromList,
-  githubMarkdownExtensions,
-  readerExtensions,
-  writerExtensions,
- )
+import Text.Pandoc
+    ( Extension(Ext_fenced_code_attributes, Ext_footnotes, 
+               Ext_gfm_auto_identifiers, Ext_implicit_header_references, 
+               Ext_smart)
+    , Extensions
+    , ReaderOptions
+    , WriterOptions(writerHighlightStyle)
+    , extensionsFromList
+    , githubMarkdownExtensions
+    , readerExtensions
+    , writerExtensions
+    )
 import Text.Pandoc.Highlighting (Style, breezeDark, styleToCss)
 
 --------------------------------------------------------------------------------
@@ -54,22 +50,22 @@ myFeedRoot = mySiteRoot
 
 config :: Configuration
 config =
-  defaultConfiguration
-    { destinationDirectory = "dist"
-    , ignoreFile = ignoreFile'
-    , previewHost = "127.0.0.1"
-    , previewPort = 8000
-    , providerDirectory = "src"
-    , storeDirectory = "ssg/_cache"
-    , tmpDirectory = "ssg/_tmp"
-    }
+    defaultConfiguration
+        { destinationDirectory = "dist"
+        , ignoreFile = ignoreFile'
+        , previewHost = "127.0.0.1"
+        , previewPort = 8000
+        , providerDirectory = "src"
+        , storeDirectory = "ssg/_cache"
+        , tmpDirectory = "ssg/_tmp"
+        }
   where
     ignoreFile' path
-      | "." `isPrefixOf` fileName = False
-      | "#" `isPrefixOf` fileName = True
-      | "~" `isSuffixOf` fileName = True
-      | ".swp" `isSuffixOf` fileName = True
-      | otherwise = False
+        | "."    `isPrefixOf` fileName = False
+        | "#"    `isPrefixOf` fileName = True
+        | "~"    `isSuffixOf` fileName = True
+        | ".swp" `isSuffixOf` fileName = True
+        | otherwise = False
       where
         fileName = takeFileName path
 
@@ -78,104 +74,103 @@ config =
 
 main :: IO ()
 main = hakyllWith config $ do
-  forM_
-    [ "CNAME"
-    , "favicon.ico"
-    , "robots.txt"
-    , "_config.yml"
-    , "images/*"
-    , "js/*"
-    , "fonts/*"
-    ]
-    $ \f -> match f $ do
-      route idRoute
-      compile copyFileCompiler
+    forM_
+        [ "CNAME"
+        , "favicon.ico"
+        , "robots.txt"
+        , "_config.yml"
+        , "images/*"
+        , "js/*"
+        , "fonts/*"
+        ] $ \f -> match f $ do
+            route idRoute
+            compile copyFileCompiler
 
-  match "css/*" $ do
-    route idRoute
-    compile compressCssCompiler
+    match "css/*" $ do
+        route idRoute
+        compile compressCssCompiler
 
-  match "posts/*" $ do
-    let ctx = constField "type" "article" <> postCtx
+    match "posts/*" $ do
+        let ctx = constField "type" "article" <> postCtx
 
-    route $ metadataRoute $ \md ->
-      customRoute $ \_ -> "blog/posts" </> fileNameFromTitle md
-    compile $
-      pandocCompilerCustom
-        >>= loadAndApplyTemplate "templates/post.html" ctx
-        >>= saveSnapshot "content"
-        >>= loadAndApplyTemplate "templates/default.html" ctx
+        route $ metadataRoute $ \md ->
+            customRoute $ \_ -> "blog/posts" </> fileNameFromTitle md
+        compile $
+            pandocCompilerCustom
+                >>= loadAndApplyTemplate "templates/post.html" ctx
+                >>= saveSnapshot "content"
+                >>= loadAndApplyTemplate "templates/default.html" ctx
 
-  match "blog/index.html" $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let blogCtx =
-            listField "posts" postCtx (return posts)
-              <> baseCtx
+    match "blog/index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let blogCtx =
+                    listField "posts" postCtx (return posts)
+                        <> baseCtx
 
-      getResourceBody
-        >>= applyAsTemplate blogCtx
-        >>= loadAndApplyTemplate "templates/default.html" blogCtx
+            getResourceBody
+                >>= applyAsTemplate blogCtx
+                >>= loadAndApplyTemplate "templates/default.html" blogCtx
 
-  match "index.html" $ do
-    route idRoute
-    compile $ do
-      posts <- fmap (take 3) . recentFirst =<< loadAll "posts/*"
-      let indexCtx =
-            listField "posts" postCtx (return posts)
-              <> baseCtx
-              <> constField "desc" "Home page"
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- fmap (take 3) . recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts)
+                        <> baseCtx
+                        <> constField "desc" "Home page"
 
-      getResourceBody
-        >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
 
-  match "about/index.html" $ do
-    route idRoute
-    compile $ do
-      getResourceBody
-        >>= applyAsTemplate baseCtx
-        >>= loadAndApplyTemplate "templates/default.html" baseCtx
+    match "about/index.html" $ do
+        route idRoute
+        compile $ do
+            getResourceBody
+                >>= applyAsTemplate baseCtx
+                >>= loadAndApplyTemplate "templates/default.html" baseCtx
 
-  match "contact/index.html" $ do
-    route idRoute
-    compile $ do
-      let contactCtx =
-            baseCtx
-              <> constField "title" "Contact"
-              <> constField "desc" "Contact page"
-      getResourceBody
-        >>= applyAsTemplate contactCtx
-        >>= loadAndApplyTemplate "templates/default.html" contactCtx
+    match "contact/index.html" $ do
+        route idRoute
+        compile $ do
+            let contactCtx = 
+                    baseCtx
+                        <> constField "title" "Contact"
+                        <> constField "desc" "Contact page"
+            getResourceBody
+                >>= applyAsTemplate contactCtx
+                >>= loadAndApplyTemplate "templates/default.html" contactCtx
 
-  match "templates/*" $ compile templateBodyCompiler
+    match "templates/*" $ compile templateBodyCompiler
 
-  create ["sitemap.xml"] $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
+    create ["sitemap.xml"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
 
-      let pages = posts
-          sitemapCtx =
-            constField "root" mySiteRoot
-              <> constField "siteName" mySiteName
-              <> listField "pages" postCtx (return pages)
+            let pages = posts
+                sitemapCtx =
+                    constField "root" mySiteRoot
+                        <> constField "siteName" mySiteName
+                        <> listField "pages" postCtx (return pages)
 
-      makeItem ("" :: String)
-        >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
+            makeItem ("" :: String)
+                >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
 
-  create ["blog/rss.xml"] $ do
-    route idRoute
-    compile (feedCompiler renderRss)
+    create ["blog/rss.xml"] $ do
+        route idRoute
+        compile (feedCompiler renderRss)
 
-  create ["blog/atom.xml"] $ do
-    route idRoute
-    compile (feedCompiler renderAtom)
+    create ["blog/atom.xml"] $ do
+        route idRoute
+        compile (feedCompiler renderAtom)
 
-  create ["css/code.css"] $ do
-    route idRoute
-    compile (makeStyle pandocHighlightStyle)
+    create ["css/code.css"] $ do
+        route idRoute
+        compile (makeStyle pandocHighlightStyle)
 
 --------------------------------------------------------------------------------
 -- COMPILER HELPERS
@@ -188,21 +183,22 @@ makeStyle = makeItem . compressCss . styleToCss
 
 baseCtx :: Context String
 baseCtx =
-  constField "root" mySiteRoot
-    <> constField "feedTitle" myFeedTitle
-    <> constField "siteName" mySiteName
-    <> defaultContext
+    constField "root" mySiteRoot
+        <> constField "feedTitle" myFeedTitle
+        <> constField "siteName" mySiteName
+        <> constField "lang" "en"
+        <> defaultContext
 
 feedCtx :: Context String
 feedCtx =
-  titleCtx
-    <> postCtx
-    <> bodyField "description"
+    titleCtx
+        <> postCtx
+        <> bodyField "description"
 
 postCtx :: Context String
 postCtx =
-  baseCtx
-    <> dateField "date" "%Y-%m-%d"
+    baseCtx
+        <> dateField "date" "%Y-%m-%d"
 
 titleCtx :: Context String
 titleCtx = field "title" updatedTitle
@@ -230,27 +226,27 @@ pandocCompilerCustom = pandocCompilerWith pandocReaderOpts pandocWriterOpts
 
 pandocExtensionsCustom :: Extensions
 pandocExtensionsCustom =
-  githubMarkdownExtensions
-    <> extensionsFromList
-      [ Ext_fenced_code_attributes
-      , Ext_gfm_auto_identifiers
-      , Ext_implicit_header_references
-      , Ext_smart
-      , Ext_footnotes
-      ]
+    githubMarkdownExtensions
+        <> extensionsFromList
+            [ Ext_fenced_code_attributes
+            , Ext_gfm_auto_identifiers
+            , Ext_implicit_header_references
+            , Ext_smart
+            , Ext_footnotes
+            ]
 
 pandocReaderOpts :: ReaderOptions
 pandocReaderOpts =
-  defaultHakyllReaderOptions
-    { readerExtensions = pandocExtensionsCustom
-    }
+    defaultHakyllReaderOptions
+        { readerExtensions = pandocExtensionsCustom
+        }
 
 pandocWriterOpts :: WriterOptions
 pandocWriterOpts =
-  defaultHakyllWriterOptions
-    { writerExtensions = pandocExtensionsCustom
-    , writerHighlightStyle = Just pandocHighlightStyle
-    }
+    defaultHakyllWriterOptions
+        { writerExtensions = pandocExtensionsCustom
+        , writerHighlightStyle = Just pandocHighlightStyle
+        }
 
 pandocHighlightStyle :: Style
 pandocHighlightStyle = breezeDark
@@ -259,30 +255,30 @@ pandocHighlightStyle = breezeDark
 -- FEEDS
 
 type FeedRenderer =
-  FeedConfiguration ->
-  Context String ->
-  [Item String] ->
-  Compiler (Item String)
+    FeedConfiguration ->
+    Context String ->
+    [Item String] ->
+    Compiler (Item String)
 
 feedCompiler :: FeedRenderer -> Compiler (Item String)
 feedCompiler renderer =
-  renderer feedConfiguration feedCtx
-    =<< recentFirst
-    =<< loadAllSnapshots "posts/*" "content"
+    renderer feedConfiguration feedCtx
+        =<< recentFirst
+        =<< loadAllSnapshots "posts/*" "content"
 
 feedConfiguration :: FeedConfiguration
 feedConfiguration =
-  FeedConfiguration
-    { feedTitle = myFeedTitle
-    , feedDescription = myFeedDescription
-    , feedAuthorName = myFeedAuthorName
-    , feedAuthorEmail = myFeedAuthorEmail
-    , feedRoot = myFeedRoot
-    }
+    FeedConfiguration
+        { feedTitle = myFeedTitle
+        , feedDescription = myFeedDescription
+        , feedAuthorName = myFeedAuthorName
+        , feedAuthorEmail = myFeedAuthorEmail
+        , feedRoot = myFeedRoot
+        }
 
 --------------------------------------------------------------------------------
 -- CUSTOM ROUTE
 
 fileNameFromTitle :: Metadata -> FilePath
 fileNameFromTitle =
-  T.unpack . (`T.append` ".html") . Slugger.toSlug . T.pack . safeTitle
+    T.unpack . (`T.append` ".html") . Slugger.toSlug . T.pack . safeTitle
