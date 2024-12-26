d623e7870fd37681664bb2608d13bbc533dffb8a
74k1 <git.t@betsumei.com>
2024-12-26 23:13:35 +0100

    feat: Add dayjs library and implement uptime counter with timezone display

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,4 @@
-//;(function() {
-//  console.log('Hello, world!');
-//})();
+// custom width calc
 
 const main = document.querySelector('main');
 
@@ -12,3 +10,52 @@ function updateWidth() {
 
 window.addEventListener('resize', updateWidth);
 updateWidth();
+
+// custom date / uptime
+
+let showSeconds = false;
+let uptimeInterval;
+
+function uptime() {
+  updateUptime();
+
+  if (!uptimeInterval) {
+    uptimeInterval = setInterval(updateUptime, 1000);
+  }
+}
+
+function updateUptime() {
+  const today = dayjs();
+  const startDate = dayjs('2002-10-11');
+  
+  if (showSeconds) {
+    const diffSeconds = today.diff(startDate, 'second');
+    document.getElementById('uptime').innerHTML = diffSeconds + 's';
+  } else {
+    const diffDays = today.diff(startDate, 'day');
+    document.getElementById('uptime').innerHTML = diffDays + 'd';
+  }
+}
+
+function toggleUptimeFormat() {
+  showSeconds = !showSeconds;
+  updateUptime();
+}
+
+//function uptime() {
+//  const today = dayjs();
+//
+//  var diffDays = today.diff('2002-10-11', 'day');
+//  document.getElementById('uptime').innerHTML = diffDays;
+//}
+
+
+function setutcnow() {
+  function updateTime() {
+    const utcPlusOne = dayjs().utc().add(1, 'hour').format('HH:mm:ss');
+    document.getElementById('UTCNOW').innerHTML = utcPlusOne;
+  }
+
+  updateTime();
+  setInterval(updateTime, 1000);
+}
